c442cdf7f01d891e7b3396380c94d1ca3bb95404
George Batchkala <george.batchkala@gmail.com>
2021-11-08 17:28:27 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TCGA Lung Dataset
---
+++ download-LUSC-and-LUAD.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Starting to download LUSC files"
+gdc-client download -m tcga-download/gdc_manifest.2021-11-03-TCGA-LUSC.txt --config tcga-download/config-LUSC.dtt
+echo "Finished downloading LUSC files"
+
+echo "Starting to download LUAD files"
+gdc-client download -m tcga-download/gdc_manifest.2021-11-03-TCGA-LUAD.txt --config tcga-download/config-LUAD.dtt
+echo "Finished downloading LUAD files"
