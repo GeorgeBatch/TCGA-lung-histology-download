72b6de36e5445be917895cd1b76c7388e0126536
George Batchkala <george.batchkala@gmail.com>
2023-10-03 15:05:28 +0100

    add real commands

--- download-LUSC-and-LUAD.sh
+++ download-LUSC-and-LUAD.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "Starting to download LUSC files"
-gdc-client download -m tcga-download/gdc_manifest.2021-11-03-TCGA-LUSC-pilot.txt --config tcga-download/config-LUSC.dtt
+# gdc-client download -m tcga-download/gdc_manifest.2021-11-03-TCGA-LUSC-pilot.txt --config tcga-download/config-LUSC.dtt
+gdc-client download -m tcga-download/gdc_manifest.2023-10-03-TCGA-LUSC.txt --config tcga-download/config-LUSC.dtt
 echo "Finished downloading LUSC files"
 
 echo "Starting to download LUAD files"
-gdc-client download -m tcga-download/gdc_manifest.2021-11-03-TCGA-LUAD-pilot.txt --config tcga-download/config-LUAD.dtt
+# gdc-client download -m tcga-download/gdc_manifest.2021-11-03-TCGA-LUAD-pilot.txt --config tcga-download/config-LUAD.dtt
+gdc-client download -m tcga-download/gdc_manifest.2023-10-03-TCGA-LUAD.txt --config tcga-download/config-LUAD.dtt
 echo "Finished downloading LUAD files"
